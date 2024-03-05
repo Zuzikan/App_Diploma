f55d16b2ec82f43acc9e05b0c36518eb36d64c4e
Zuza Bal <ul0255160@edu.uni.lodz.pl>
2024-03-06 00:29:54 +0100

    podstrona obiczenia pisana w testach

--- testy.py
+++ testy.py
@@ -1,18 +1,115 @@
-from sympy import sympify, simplify
+import sys
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QApplication, QWidget, QGridLayout, QLabel, QComboBox, QLineEdit, QSlider, QHBoxLayout,
+                             QPushButton, QDialog)
+from PyQt5.QtGui import QIntValidator
 
-# Example expression
-expr_string = "x**2 + 3*x - 1/2 + acos(x)"
 
-# Parsing the string to a sympy expression
-expr = sympify(expr_string)
+class Oblicz(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-# Display the parsed expression
-print(f"Parsed Expression: {expr}")
+    def initUI(self):
+        layout = QGridLayout()
+        sliderLayout = QHBoxLayout()
+        abHorizontal = QHBoxLayout()
+        nodeHorizontal = QHBoxLayout()
 
-# Evaluate the expression for a specific value of x
-x_value =0.23
-evaluated_expr = expr.subs('x', x_value)
-numeric_value = evaluated_expr.evalf()
-print(f"Evaluated at x={x_value}: {evaluated_expr}")
-print(f" x={x_value}: {numeric_value}")
+        combo = QComboBox(self)
+        combo.addItems(["Page 1", "Page 2"])
+        l1 = QLabel("Porównaj z: ", self)
+        l2 = QLabel("Wpisz równanie: ", self)
+        l3 = QLabel("Podaj przedział [a,b]:", self)
+        rownanie = QLineEdit(self)
+        instrukcja = QPushButton('Instrukcja', self)
+        la = QLabel("a: ", self)
+        lb = QLabel("b:", self)
+        a = QLineEdit(self)
+        b = QLineEdit(self)
+        # l5 = QLabel("n: ", self)
+        l6 = QLabel("Wynik: ", self)
+        slider = QSlider(Qt.Horizontal, self)
+        slider.setMinimum(2)
+        slider.setMaximum(50)
+        start = QLabel('2')
+        end = QLabel('50')
+        oblicz = QPushButton('Oblicz', self)
+        self.wartosc = QLabel("Liczba node'ów: 2", self)
 
+        slider.valueChanged.connect(self.sliderValueChanged)
+        instrukcja.setStyleSheet("border-radius : 5px; background-color : #CCDDFF")
+        oblicz.setStyleSheet("border-radius : 5px; background-color : #CCDDFF")
+        slider.setStyleSheet("""
+                QSlider::handle:horizontal {
+                    background: #333;
+                    border: 1px solid #555;
+                    width: 8px;
+                    height: 8px;
+                    margin: -8px 0;  
+                    border-radius:4px;  
+                }
+                """)
+        a.setPlaceholderText("Wpisz wartość a")
+        b.setPlaceholderText("Wpisz wartość b")
+        a.setValidator(QIntValidator())
+        b.setValidator(QIntValidator())
+        rownanie.setPlaceholderText("Wpisz wartość całki")
+        l3.setAlignment(Qt.AlignCenter)
+        self.wartosc.setAlignment(Qt.AlignCenter)
+
+        # Connect the combo box's signal to the slot
+        # self.combo.activated[str].connect(self.onActivated)
+        layout.addWidget(l1, 0, 0)
+        layout.addWidget(combo, 0, 1)
+
+        layout.addWidget(l2, 1, 0)
+        layout.addWidget(rownanie, 1, 1)
+        layout.addWidget(instrukcja, 2, 0, 1, 2)
+
+        layout.addWidget(l3, 3, 0, 1, 2)
+
+        abHorizontal.addWidget(la)
+        abHorizontal.addWidget(a)
+
+        abHorizontal.addWidget(lb)
+        abHorizontal.addWidget(b)
+
+        layout.addLayout(abHorizontal, 4, 0, 1, 2)
+
+        layout.addWidget(oblicz, 5, 0, 1, 2)
+
+        layout.addWidget(self.wartosc, 6, 0, 1, 2)
+
+        sliderLayout.addWidget(start)
+        sliderLayout.addWidget(slider)
+        sliderLayout.addWidget(end)
+
+        sliderLayout.setStretch(0, 1)
+        sliderLayout.setStretch(1, 50)
+        sliderLayout.setStretch(2, 1)
+
+        layout.addLayout(sliderLayout, 7, 0, 1, 2)
+
+        layout.addWidget(l6, 7, 3, 1, 2)
+
+        self.setLayout(layout)
+        self.setWindowTitle('Oblicz')
+
+    def onActivated(self, text):
+        self.label.setText(f"You selected: {text}")
+        self.label.adjustSize()
+
+    def sliderValueChanged(self, value):
+        self.wartosc.setText(f"Liczba node'ów: {value}")
+
+
+def main():
+    app = QApplication(sys.argv)
+    ex = Oblicz()
+    ex.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
