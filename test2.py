74eff4dd9000c7c53a1c14d7c10b69955e344929
Zuza Bal <ul0255160@edu.uni.lodz.pl>
2024-03-07 22:07:53 +0100

    zrobienie całego oblicz

--- test2.py
+++ test2.py
@@ -1,12 +1,15 @@
 import sys
+import numpy as np
+import timeit
 import math
-from PyQt5.QtWidgets import QApplication, QDialog, QGridLayout, QLabel, QLineEdit, QPushButton, QSlider, QHBoxLayout
-from PyQt5.QtCore import Qt
 from sympy import symbols, sympify, lambdify
+from PyQt5.QtCore import Qt, QCoreApplication
+from PyQt5.QtWidgets import (QApplication, QGridLayout, QLabel, QComboBox, QLineEdit, QSlider, QHBoxLayout,
+                             QPushButton, QDialog, QTabWidget, QVBoxLayout, QWidget)
+from PyQt5.QtGui import QDoubleValidator, QFont
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 from matplotlib.patches import Rectangle
-import numpy as np
 from sympy.core.sympify import SympifyError
 
 
@@ -16,72 +19,152 @@ class Oblicz(QDialog):
         self.initUI()
 
     def initUI(self):
-        self.layout = QGridLayout()
-        self.sliderLayout = QHBoxLayout()
-        self.abHorizontal = QHBoxLayout()
 
+        # self.setStyleSheet("background-color: white;")
+        font = QFont()
+        font.setPointSize(12)
+
+        layout = QGridLayout()
+        sliderLayout = QHBoxLayout()
+        abHorizontal = QHBoxLayout()
+        layout_for_buttons = QHBoxLayout()
+
+        combo = QComboBox(self)
+        combo.addItems(["Page 1", "Page 2"])
+        l1 = QLabel("Porównaj z: ", self)
         l2 = QLabel("Wpisz równanie: ", self)
+        l3 = QLabel("Podaj przedział [a,b]:", self)
         self.rownanie = QLineEdit(self)
-        self.error_occured = False
-        self.l6 = QLabel("Wynik: ", self)
-        self.l7 = QLabel("Wynik: ", self)
-        self.oblicz = QPushButton('Oblicz', self)
-
-        #self.check = QPushButton("Check", self)
-
-        self.slider = QSlider(Qt.Horizontal, self)
-        self.slider.setMinimum(2)
-        self.slider.setMaximum(50)
-        self.slider.setValue(2)
-        self.n = 2
-        start = QLabel('2')
-        end = QLabel('50')
-        self.wartosc = QLabel("Liczba node'ów: 2", self)
-
+        instrukcja = QPushButton('Instrukcja', self)
         la = QLabel("a: ", self)
         lb = QLabel("b:", self)
         self.a = QLineEdit(self)
         self.b = QLineEdit(self)
+        self.n = 2
+        self.l6 = QLabel("Wynik midpoint: ", self)
+        self.l6l = QLabel("Wynik left side: ", self)
+        self.l6r = QLabel("Wynik right side: ", self)
+        self.l7 = QLabel(self)
+        self.l7l = QLabel(self)
+        self.l7r = QLabel(self)
+        self.slider = QSlider(Qt.Horizontal, self)
+        self.slider.setMinimum(2)
+        self.slider.setMaximum(50)
+        self.slider.setValue(2)
+        start = QLabel('2')
+        end = QLabel('50')
+        oblicz = QPushButton('Oblicz', self)
+        self.wartosc = QLabel("Liczba node'ów: 2", self)
+        self.figure1 = Figure()
+        self.canvas1 = FigureCanvas(self.figure1)
+        self.figure2 = Figure()
+        self.canvas2 = FigureCanvas(self.figure2)
+        self.figure3 = Figure()
+        self.canvas3 = FigureCanvas(self.figure3)
 
-        self.figure = Figure()
-        self.canvas = FigureCanvas(self.figure)
-
-        self.oblicz.setStyleSheet("border-radius : 5px; background-color : #CCDDFF")
-
-        self.layout.addWidget(l2, 0, 0)
-        self.layout.addWidget(self.rownanie, 1, 0)
-        #self.layout.addWidget(self.oblicz, 6, 0)
-        self.layout.addWidget(self.oblicz, 6, 0)
-        self.layout.addWidget(self.l6, 2, 0)
-        self.layout.addWidget(self.wartosc, 3, 0)
-
-        self.abHorizontal.addWidget(la)
-        self.abHorizontal.addWidget(self.a)
-
-        self.abHorizontal.addWidget(lb)
-        self.abHorizontal.addWidget(self.b)
-
-        self.layout.addLayout(self.abHorizontal, 4, 0, 1, 2)
-
-        self.sliderLayout.addWidget(start)
-        self.sliderLayout.addWidget(self.slider)
-        self.sliderLayout.addWidget(end)
-
-        self.layout.addLayout(self.sliderLayout, 5, 0, 1, 2)
-        self.layout.addWidget(self.l7, 8, 0)
-
-        # Connection
         self.slider.valueChanged.connect(self.slider_nodes)
-        self.oblicz.clicked.connect(self.check_errors)
-        #self.check.clicked.connect(self.check_errors)
-        self.layout.addWidget(self.canvas, 0, 2, 5, 2)
+        instrukcja.setStyleSheet("border-radius : 5px; background-color : #CCDDFF")
+        oblicz.setStyleSheet("border-radius : 5px; background-color : #CCDDFF")
+        self.slider.setStyleSheet("""
+                QSlider::handle:horizontal {
+                    background: #333;
+                    border: 1px solid #555;
+                    width: 8px;
+                    height: 8px;
+                    margin: -8px 0;  
+                    border-radius:4px;  
+                }
+                """)
+        self.a.setPlaceholderText("Wpisz wartość a")
+        self.b.setPlaceholderText("Wpisz wartość b")
+        self.a.setValidator(QDoubleValidator())
+        self.b.setValidator(QDoubleValidator())
+        self.rownanie.setPlaceholderText("Wpisz wartość całki")
+        l3.setAlignment(Qt.AlignCenter)
+        self.wartosc.setAlignment(Qt.AlignCenter)
 
+        # Connect the combo box's signal to the slot
+        # self.combo.activated[str].connect(self.onActivated)
+        layout.addWidget(l1, 3, 0)
+        layout.addWidget(combo, 3, 1)
 
+        layout.addWidget(l2, 4, 0)
+        layout.addWidget(self.rownanie, 4, 1)
+        layout.addWidget(instrukcja, 5, 0, 1, 2)
 
+        layout.addWidget(l3, 6, 0, 1, 2)
 
+        abHorizontal.addWidget(la)
+        abHorizontal.addWidget(self.a)
 
-        self.setLayout(self.layout)
+        abHorizontal.addWidget(lb)
+        abHorizontal.addWidget(self.b)
+
+        layout.addLayout(abHorizontal, 7, 0, 1, 2)
+
+        layout.addWidget(oblicz, 8, 0, 1, 2)
+        oblicz.clicked.connect(self.check_errors)
+
+        layout.addWidget(self.wartosc, 9, 0, 1, 2)
+
+        sliderLayout.addWidget(start)
+        sliderLayout.addWidget(self.slider)
+        sliderLayout.addWidget(end)
+
+        sliderLayout.setStretch(0, 1)
+        sliderLayout.setStretch(1, 50)
+        sliderLayout.setStretch(2, 1)
+
+        layout.addLayout(sliderLayout, 10, 0, 1, 2)
+
+        layout.addWidget(self.l6, 11, 0, 1, 2)
+        layout.addWidget(self.l6l, 12, 0, 1, 2)
+        layout.addWidget(self.l6r, 13, 0, 1, 2)
+        layout.addWidget(self.l7, 14, 0, 1, 2)
+        layout.addWidget(self.l7l, 15, 0, 1, 2)
+        layout.addWidget(self.l7r, 16, 0, 1, 2)
+
+        # tab for canvas
+        self.tabWidget = QTabWidget(self)
+        self.tab1 = QWidget()
+        self.tab1.layout = QVBoxLayout(self.tab1)
+        self.tab1.layout.addWidget(self.canvas1)
+
+        self.tab2 = QWidget()
+        self.tab2.layout = QVBoxLayout(self.tab2)
+        self.tab2.layout.addWidget(self.canvas2)
+
+        self.tab3 = QWidget()
+        self.tab3.layout = QVBoxLayout(self.tab3)
+        self.tab3.layout.addWidget(self.canvas3)
+
+        self.tabWidget.addTab(self.tab1, "Tab 1")
+        self.tabWidget.addTab(self.tab2, "Tab 2")
+        self.tabWidget.addTab(self.tab3, "Tab 3")
+
+        # layout.addWidget(self.canvas, 0, 3, 15, 1)
+        layout.addWidget(self.tabWidget, 0, 3, 15, 1)
+
+        zamknij = QPushButton('Zamknij program')
+        zamknij_okno = QPushButton("Zamknij okno")
+
+        zamknij_okno.clicked.connect(self.close)
+        zamknij.clicked.connect(QCoreApplication.instance().quit)
+
+        zamknij.setStyleSheet("border-radius : 5px; background-color : #FCDDDD")
+        zamknij_okno.setStyleSheet("border-radius : 5px; background-color : #FCDDDD")
+
+        layout_for_buttons.addWidget(zamknij)
+        layout_for_buttons.addWidget(zamknij_okno)
+
+        layout.addLayout(layout_for_buttons, 18, 0, 1, 2)
+
+        self.setLayout(layout)
         self.setWindowTitle('Oblicz')
+
+    def onActivated(self, text):
+        self.label.setText(f"You selected: {text}")
+        self.label.adjustSize()
 
     def check_errors(self):
         try:
@@ -95,6 +178,17 @@ class Oblicz(QDialog):
             self.error_occured = True
             return e
 
+    def check_errors_leftside(self):
+        try:
+            self.f(1)
+        except Exception as e:
+            self.l6.setText(f"Error: Nieprawidłowe równanie. Sprawdź wpisane dane.")
+            self.error_occured = True
+        try:
+            self.get_a_b()
+        except Exception as e:
+            self.error_occured = True
+            return e
 
     def f(self, x):
         rownanie_string = self.rownanie.text()
@@ -115,12 +209,32 @@ class Oblicz(QDialog):
             self.l6.setText("Error: Podana została zła funkcja. Sprawdź wpisane dane.")
             return None
 
-    def metoda_prostokatow(self, n, a, b):
+    def metoda_prostokatow_midpoint(self, n, a, b):
         try:
             h = (b - a) / n
             wynik = 0
             for i in range(n):
                 xi = a + (i + 0.5) * h
+                wynik += self.f(xi) * h
+
+            if math.isnan(wynik):
+                self.l6.setText("Error: Podana została zła funkcja lub jej przedziały.")
+                self.error_occured = True
+                return None
+            else:
+                self.l6.setText(f"Wynik: {wynik}")
+
+        except Exception as e:
+            self.l6.setText(f"Error: Problem z obliczeniem wartości funkcji.")
+            self.error_occured = True
+            return e
+
+    def metoda_prostokatow_leftside(self, n, a, b):
+        try:
+            h = (b - a) / n
+            wynik = 0
+            for i in range(n):
+                xi = a + i * h
                 wynik += self.f(xi) * h
 
             if math.isnan(wynik):
@@ -142,53 +256,122 @@ class Oblicz(QDialog):
 
     def get_a_b(self):
         if self.rownanie.text().strip() == "":
-            self.l6.setText("Error: Wpisz równanie.")
-            self.error_occured = True
+            self.l6.setText("Error: Wpisz równanie")
             return
         if self.a.text().strip() == "":
-            self.l6.setText("Error: a nie może być puste.")
-            self.error_occured = True
+            self.l6.setText("Error: a nie może być puste")
             return
         if self.b.text().strip() == "":
-            self.l6.setText("Error: b nie może być puste.")
-            self.error_occured = True
+            self.l6.setText("Error: b nie może być puste")
             return
         try:
             a = int(self.a.text())
             b = int(self.b.text())
         except ValueError:
             self.l6.setText("Error: Nieprawidłowe dane wejściowe dla a lub b.")
-            self.error_occured = True
             return
         if a >= b:
             self.l6.setText("Error: a powinno być mniejsze niż b.")
-            self.error_occured = True
             return
-        self.metoda_prostokatow(self.n, a, b)
-        self.update_wykres(a, b)
 
-    def update_wykres(self, a, b):
+        start_time = timeit.default_timer()
+        self.metoda_prostokatow_midpoint(self.n, a, b)
+        end_time = timeit.default_timer()
+        time = end_time - start_time
+        self.l7.setText(f"Czas potrzebny do obliczenia: {time}")
+
+        start_time = timeit.default_timer()
+        self.metoda_prostokatow_midpoint(self.n, a, b)
+        end_time = timeit.default_timer()
+        time = end_time - start_time
+        self.l7.setText(f"Czas potrzebny do obliczenia: {time}")
+
+        start_time = timeit.default_timer()
+        self.metoda_prostokatow_midpoint(self.n, a, b)
+        end_time = timeit.default_timer()
+        time = end_time - start_time
+        self.l7.setText(f"Czas potrzebny do obliczenia: {time}")
+
+        self.update_wykres_midpoint(a, b)
+        self.update_wykres_leftside(a, b)
+
+    def get_a_b_leftside(self):
+        if self.rownanie.text().strip() == "":
+            self.l6.setText("Error: Wpisz równanie")
+            return
+        if self.a.text().strip() == "":
+            self.l6.setText("Error: a nie może być puste")
+            return
+        if self.b.text().strip() == "":
+            self.l6.setText("Error: b nie może być puste")
+            return
+        try:
+            a = int(self.a.text())
+            b = int(self.b.text())
+        except ValueError:
+            self.l6.setText("Error: Nieprawidłowe dane wejściowe dla a lub b.")
+            return
+        if a >= b:
+            self.l6.setText("Error: a powinno być mniejsze niż b.")
+            return
+
+        start_time = timeit.default_timer()
+        self.metoda_prostokatow_leftside(self.n, a, b)
+        end_time = timeit.default_timer()
+        time = end_time - start_time
+        self.l7.setText(f"Czas potrzebny do obliczenia: {time}")
+        self.update_wykres_leftside(a, b)
+
+    def update_wykres_midpoint(self, a, b):
         if a is None or b is None or a >= b:
             return
 
         self.figure.clear()
         ax = self.figure.add_subplot(111)
-        ax.set_xlim(a, b)
+        # ax.set_xlim(a, b)
         h = (b - a) / self.n
         x = [a + i * h for i in range(self.n)]
         y = [self.f(a + (i + 0.5) * h) for i in range(self.n)]
+        ax.grid(True, alpha=0.2)
         y_max = max(y)
-        ax.set_ylim(0, y_max + y_max * 0.1)
+        # ax.set_ylim(0, y_max + y_max * 0.1)
 
         for i in range(self.n):
             rect = Rectangle((x[i], 0), h, y[i], linewidth=1, edgecolor='r', facecolor='r',
                              alpha=0.5)
             ax.add_patch(rect)
 
-        x_f = np.linspace(a, b, 300)
+        start = a - 2
+        stop = b + 2
+
+        x_f = np.linspace(start, stop, 300)
+        y_f = self.f(x_f)
+        ax.plot(x_f, y_f, 'b-', linewidth=1)
+        self.canvas1.draw()
+
+    def update_wykres_leftside(self, a, b):
+        if a is None or b is None or a >= b:
+            return
+
+        self.figure.clear()
+        ax = self.figure.add_subplot(111)
+        # ax.set_xlim(a, b)
+        h = (b - a) / self.n
+        x_left = [a + i * h for i in range(self.n)]
+        y_left = [self.f(x) for x in x_left]
+        ax.grid(True, alpha=0.2)
+        y_max = max(y_left) if y_left else 0
+        for i in range(len(x_left)):
+            rect = Rectangle((x_left[i], 0), h, y_left[i], linewidth=1, edgecolor='r', facecolor='r', alpha=0.5)
+            ax.add_patch(rect)
+
+        start, stop = a - 2, b + 2
+        x_f = np.linspace(start, stop, 300)
         y_f = [self.f(x) for x in x_f]
         ax.plot(x_f, y_f, 'b-', linewidth=1)
-        self.canvas.draw()
+        ax.set_xlim(start, stop)
+        ax.set_ylim(min(0, min(y_f)), y_max + y_max * 0.1)
+        self.canvas2.draw()
 
 
 def main():
--- testy.py
+++ testy.py
@@ -5,7 +5,7 @@ import math
 from sympy import symbols, sympify, lambdify
 from PyQt5.QtCore import Qt, QCoreApplication
 from PyQt5.QtWidgets import (QApplication, QGridLayout, QLabel, QComboBox, QLineEdit, QSlider, QHBoxLayout,
-                             QPushButton, QDialog, QTabWidget)
+                             QPushButton, QDialog, QTabWidget, QVBoxLayout, QWidget)
 from PyQt5.QtGui import QDoubleValidator, QFont
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
@@ -18,7 +18,6 @@ class Oblicz(QDialog):
         super().__init__()
         self.initUI()
 
-
     def initUI(self):
 
         # self.setStyleSheet("background-color: white;")
@@ -29,6 +28,7 @@ class Oblicz(QDialog):
         sliderLayout = QHBoxLayout()
         abHorizontal = QHBoxLayout()
         layout_for_buttons = QHBoxLayout()
+
 
         combo = QComboBox(self)
         combo.addItems(["Page 1", "Page 2"])
@@ -42,8 +42,12 @@ class Oblicz(QDialog):
         self.a = QLineEdit(self)
         self.b = QLineEdit(self)
         self.n = 2
-        self.l6 = QLabel("Wynik: ", self)
+        self.l6 = QLabel(self)
+        self.l6l = QLabel(self)
+        self.l6r = QLabel(self)
         self.l7 = QLabel(self)
+        self.l7l = QLabel(self)
+        self.l7r = QLabel(self)
         self.slider = QSlider(Qt.Horizontal, self)
         self.slider.setMinimum(2)
         self.slider.setMaximum(50)
@@ -52,8 +56,12 @@ class Oblicz(QDialog):
         end = QLabel('50')
         oblicz = QPushButton('Oblicz', self)
         self.wartosc = QLabel("Liczba node'ów: 2", self)
-        self.figure = Figure()
-        self.canvas = FigureCanvas(self.figure)
+        self.figure1 = Figure()
+        self.canvas1 = FigureCanvas(self.figure1)
+        self.figure2 = Figure()
+        self.canvas2 = FigureCanvas(self.figure2)
+        self.figure3 = Figure()
+        self.canvas3 = FigureCanvas(self.figure3)
 
         self.slider.valueChanged.connect(self.slider_nodes)
         instrukcja.setStyleSheet("border-radius : 5px; background-color : #CCDDFF")
@@ -78,14 +86,14 @@ class Oblicz(QDialog):
 
         # Connect the combo box's signal to the slot
         # self.combo.activated[str].connect(self.onActivated)
-        layout.addWidget(l1, 3, 0)
-        layout.addWidget(combo, 3, 1)
+        layout.addWidget(l1, 1, 0)
+        layout.addWidget(combo, 1, 1)
 
-        layout.addWidget(l2, 4, 0)
-        layout.addWidget(self.rownanie, 4, 1)
-        layout.addWidget(instrukcja, 5, 0, 1, 2)
+        layout.addWidget(l2, 2, 0)
+        layout.addWidget(self.rownanie, 2, 1)
+        layout.addWidget(instrukcja, 3, 0, 1, 2)
 
-        layout.addWidget(l3, 6, 0, 1, 2)
+        layout.addWidget(l3, 4, 0, 1, 2)
 
         abHorizontal.addWidget(la)
         abHorizontal.addWidget(self.a)
@@ -93,12 +101,12 @@ class Oblicz(QDialog):
         abHorizontal.addWidget(lb)
         abHorizontal.addWidget(self.b)
 
-        layout.addLayout(abHorizontal, 7, 0, 1, 2)
+        layout.addLayout(abHorizontal, 5, 0, 1, 2)
 
-        layout.addWidget(oblicz, 8, 0, 1, 2)
+        layout.addWidget(oblicz, 6, 0, 1, 2)
         oblicz.clicked.connect(self.check_errors)
 
-        layout.addWidget(self.wartosc, 9, 0, 1, 2)
+        layout.addWidget(self.wartosc, 7, 0, 1, 2)
 
         sliderLayout.addWidget(start)
         sliderLayout.addWidget(self.slider)
@@ -108,12 +116,35 @@ class Oblicz(QDialog):
         sliderLayout.setStretch(1, 50)
         sliderLayout.setStretch(2, 1)
 
-        layout.addLayout(sliderLayout, 10, 0, 1, 2)
+        layout.addLayout(sliderLayout, 8, 0, 1, 2)
 
-        layout.addWidget(self.l6, 11, 0, 1, 2)
+        layout.addWidget(self.l6, 9, 0, 1, 2)
+        layout.addWidget(self.l6l, 10, 0, 1, 2)
+        layout.addWidget(self.l6r, 11, 0, 1, 2)
         layout.addWidget(self.l7, 12, 0, 1, 2)
+        layout.addWidget(self.l7l, 13, 0, 1, 2)
+        layout.addWidget(self.l7r, 14, 0, 1, 2)
 
-        layout.addWidget(self.canvas, 0, 3, 15, 1)
+        #tab for canvas
+        self.tabWidget = QTabWidget(self)
+        self.tab1 = QWidget()
+        self.tab1.layout = QVBoxLayout(self.tab1)
+        self.tab1.layout.addWidget(self.canvas1)
+
+        self.tab2 = QWidget()
+        self.tab2.layout = QVBoxLayout(self.tab2)
+        self.tab2.layout.addWidget(self.canvas2)
+
+        self.tab3 = QWidget()
+        self.tab3.layout = QVBoxLayout(self.tab3)
+        self.tab3.layout.addWidget(self.canvas3)
+
+        self.tabWidget.addTab(self.tab1, "Midpoint")
+        self.tabWidget.addTab(self.tab2, "Left side")
+        self.tabWidget.addTab(self.tab3, "Right side")
+
+        #layout.addWidget(self.canvas, 0, 3, 15, 1)
+        layout.addWidget(self.tabWidget, 0, 3, 15, 1)
 
         zamknij = QPushButton('Zamknij program')
         zamknij_okno = QPushButton("Zamknij okno")
@@ -127,16 +158,29 @@ class Oblicz(QDialog):
         layout_for_buttons.addWidget(zamknij)
         layout_for_buttons.addWidget(zamknij_okno)
 
-        layout.addLayout(layout_for_buttons, 16, 0, 1, 2)
+        layout.addLayout(layout_for_buttons, 20, 0, 1, 2)
 
         self.setLayout(layout)
         self.setWindowTitle('Oblicz')
+
 
     def onActivated(self, text):
         self.label.setText(f"You selected: {text}")
         self.label.adjustSize()
 
     def check_errors(self):
+        try:
+            self.f(1)
+        except Exception as e:
+            self.l6.setText(f"Error: Nieprawidłowe równanie. Sprawdź wpisane dane.")
+            self.error_occured = True
+        try:
+            self.get_a_b()
+        except Exception as e:
+            self.error_occured = True
+            return e
+
+    def check_errors_leftside(self):
         try:
             self.f(1)
         except Exception as e:
@@ -167,7 +211,7 @@ class Oblicz(QDialog):
             self.l6.setText("Error: Podana została zła funkcja. Sprawdź wpisane dane.")
             return None
 
-    def metoda_prostokatow(self, n, a, b):
+    def metoda_prostokatow_midpoint(self, n, a, b):
         try:
             h = (b - a) / n
             wynik = 0
@@ -180,13 +224,52 @@ class Oblicz(QDialog):
                 self.error_occured = True
                 return None
             else:
-                self.l6.setText(f"Wynik: {wynik}")
+                self.l6.setText(f"Wynik dla midpoint: {wynik}")
 
         except Exception as e:
             self.l6.setText(f"Error: Problem z obliczeniem wartości funkcji.")
             self.error_occured = True
             return e
 
+    def metoda_prostokatow_leftside(self, n, a, b):
+        try:
+            h = (b - a) / n
+            wynik = 0
+            for i in range(n):
+                xi = a + i * h
+                wynik += self.f(xi) * h
+
+            if math.isnan(wynik):
+                self.l6.setText("Error: Podana została zła funkcja lub jej przedziały.")
+                self.error_occured = True
+                return None
+            else:
+                self.l6l.setText(f"Wynik da leftside: {wynik}")
+
+        except Exception as e:
+            self.l6.setText(f"Error: Problem z obliczeniem wartości funkcji.")
+            self.error_occured = True
+            return e
+
+    def metoda_prostokatow_rightside(self, n, a, b):
+        try:
+            h = (b - a) / n
+            wynik = 0
+            for i in range(n+1):
+                xi = a + i * h
+                wynik += self.f(xi) * h
+
+            if math.isnan(wynik):
+                self.l6.setText("Error: Podana została zła funkcja lub jej przedziały.")
+                self.error_occured = True
+                return None
+            else:
+                self.l6r.setText(f"Wynik rightside: {wynik}")
+
+        except Exception as e:
+            self.l6.setText(f"Error: Problem z obliczeniem wartości funkcji.")
+            self.error_occured = True
+            return e
 
     def slider_nodes(self, value):
         self.n = value
@@ -214,18 +297,61 @@ class Oblicz(QDialog):
             return
 
         start_time = timeit.default_timer()
-        self.metoda_prostokatow(self.n, a, b)
+        self.metoda_prostokatow_midpoint(self.n, a, b)
+        end_time = timeit.default_timer()
+        time = end_time - start_time
+        self.l7.setText(f"Czas potrzebny do obliczenia midpoint: {time}")
+
+        start_timel = timeit.default_timer()
+        self.metoda_prostokatow_leftside(self.n, a, b)
+        end_timel = timeit.default_timer()
+        timel = end_timel - start_timel
+        self.l7l.setText(f"Czas potrzebny do obliczenia left side: {timel}")
+
+        start_timer = timeit.default_timer()
+        self.metoda_prostokatow_rightside(self.n, a, b)
+        end_timer = timeit.default_timer()
+        timer = end_timer - start_timer
+        self.l7r.setText(f"Czas potrzebny do obliczenia midpoint: {timer}")
+
+
+        self.update_wykres_midpoint(a, b)
+        self.update_wykres_leftside(a, b)
+        self.update_wykres_rightside(a, b)
+
+    def get_a_b_leftside(self):
+        if self.rownanie.text().strip() == "":
+            self.l6.setText("Error: Wpisz równanie")
+            return
+        if self.a.text().strip() == "":
+            self.l6.setText("Error: a nie może być puste")
+            return
+        if self.b.text().strip() == "":
+            self.l6.setText("Error: b nie może być puste")
+            return
+        try:
+            a = int(self.a.text())
+            b = int(self.b.text())
+        except ValueError:
+            self.l6.setText("Error: Nieprawidłowe dane wejściowe dla a lub b.")
+            return
+        if a >= b:
+            self.l6.setText("Error: a powinno być mniejsze niż b.")
+            return
+
+        start_time = timeit.default_timer()
+        self.metoda_prostokatow_leftside(self.n, a, b)
         end_time = timeit.default_timer()
         time = end_time - start_time
         self.l7.setText(f"Czas potrzebny do obliczenia: {time}")
-        self.update_wykres(a, b)
+        self.update_wykres_leftside(a, b)
 
-    def update_wykres(self, a, b):
+    def update_wykres_midpoint(self, a, b):
         if a is None or b is None or a >= b:
             return
 
-        self.figure.clear()
-        ax = self.figure.add_subplot(111)
+        self.figure1.clear()
+        ax = self.figure1.add_subplot(111)
         # ax.set_xlim(a, b)
         h = (b - a) / self.n
         x = [a + i * h for i in range(self.n)]
@@ -245,7 +371,49 @@ class Oblicz(QDialog):
         x_f = np.linspace(start, stop, 300)
         y_f = self.f(x_f)
         ax.plot(x_f, y_f, 'b-', linewidth=1)
-        self.canvas.draw()
+        self.canvas1.draw()
+
+    def update_wykres_leftside(self, a, b):
+        if a is None or b is None or a >= b:
+            return
+
+        self.figure2.clear()
+        ax = self.figure2.add_subplot(111)
+        # ax.set_xlim(a, b)
+        h = (b - a) / self.n
+        x_left = [a + i * h for i in range(self.n)]
+        y_left = [self.f(x) for x in x_left]
+        ax.grid(True, alpha=0.2)
+        y_max = max(y_left) if y_left else 0
+        for i in range(len(x_left)):
+            rect = Rectangle((x_left[i], 0), h, y_left[i], linewidth=1, edgecolor='r', facecolor='r', alpha=0.5)
+            ax.add_patch(rect)
+
+        start, stop = a - 2, b + 2
+        x_f = np.linspace(start, stop, 300)
+        y_f = self.f(x_f)
+        ax.plot(x_f, y_f, 'b-', linewidth=1)
+        self.canvas2.draw()
+
+    def update_wykres_rightside(self, a, b):
+        if a is None or b is None or a >= b:
+            return
+
+        self.figure3.clear()
+        ax = self.figure3.add_subplot(111)
+        h = (b - a) / self.n
+        x_right = [a + (i + 1) * h for i in range(self.n)]
+        y_right = [self.f(x) for x in x_right]
+        ax.grid(True, alpha=0.2)
+        for i in range(self.n):
+            rect = Rectangle((x_right[i] - h, 0), h, y_right[i], linewidth=1, edgecolor='r', facecolor='r', alpha=0.5)
+            ax.add_patch(rect)
+
+        start, stop = a - 2, b + 2
+        x_f = np.linspace(start, stop, 300)
+        y_f = self.f(x_f)
+        ax.plot(x_f, y_f, 'b-', linewidth=1)
+        self.canvas3.draw()
 
 
 def main():
