f058c149b2aaf394cb62235824d35e0898d3b369
Zuza Bal <ul0255160@edu.uni.lodz.pl>
2024-04-01 03:53:35 +0200

    czebyszew

--- test2.py
+++ test2.py
@@ -1,26 +1,20 @@
-from cmath import acos
+import numpy as np
 
-import sympy
-from sympy import symbols, S, sqrt, log, solveset, Interval, acos
 
-x = symbols('x')
+def chebyshev_nodes(n):
+    return np.cos(0.5 * np.pi * (2 * np.arange(1, n + 1) - 1) / n)
 
-# Dla funkcji z pierwiastkiem kwadratowym
-expr1_domain = solveset(sqrt(x - 2) > 0, x, domain=S.Reals)
 
-# Dla funkcji logarytmicznej
-expr2_domain = solveset(2/(x-3) > 0, x, domain=S.Reals)
+def chebyshev_solver(f, a, b, n):
+    d = (b - a)
+    c = 0.5 * np.pi * d / n
 
-# Przygotowanie formatowania na przedziały (a, b)
-def format_interval(interval):
-    if isinstance(interval, Interval):
-        return f"({interval.start}; {interval.end})"
-    else:
-        # Jeśli rozwiązanie nie jest przedziałem, zwróć oryginalny format
-        return str(interval)
+    cn = chebyshev_nodes(n)
+    v = 0.5 * (cn + 1) * d + a
+    return c * np.sum(f(v) * np.sqrt(1 - cn**2))
 
-# Formatowanie i wyświetlanie wyników
-expr1_domain_formatted = format_interval(expr1_domain)
-expr2_domain_formatted = format_interval(expr2_domain)
 
-print(expr1_domain_formatted, expr2_domain_formatted)
+def efunc(x):
+    return np.sqrt(3 * x**2 + 7) / (5 + np.log(2*x))
+
+print(chebyshev_solver(efunc, 2, 3, 3))
